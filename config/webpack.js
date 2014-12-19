9071f2dc715c2ccdcfefc46f7047931a3851eb21
Ivan Shornikov <paraboom@gmail.com>
2014-12-19 14:33:44 +0700

    Fix test name

--- config/webpack.js
+++ config/webpack.js
@@ -3,8 +3,8 @@ var path = require('path');
 var config = {
   cache: true,
   entry: {
-    'sniff_fns': 'src/sniff_fns.js',
-    'sniff_fns_global': './src/sniff_fns_global.js'
+    // 'sniff_fns': 'src/sniff_fns.js',
+    // 'sniff_fns_global': './src/sniff_fns_global.js'
   },
   output: {
     path: path.join(process.cwd(), 'dist'),
