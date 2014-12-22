e3dc40228c3026522e3bb724d5ac573d2f61f344
Ivan Shornikov <paraboom@gmail.com>
2014-12-22 18:26:37 +0700

    Fixes for global bundle

---
+++ dist/sniff_fns.js
@@ -0,0 +1,312 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(module) {var isChrome = __webpack_require__(1);
+	var isChromeMobile = __webpack_require__(2);
+	var isAndroid = __webpack_require__(3);
+	var isAndroidMobile = __webpack_require__(4);
+	var isBB = __webpack_require__(5);
+	var isFF = __webpack_require__(6);
+	var isOpera = __webpack_require__(7);
+	var isSafari = __webpack_require__(8);
+	var isIphone = __webpack_require__(9);
+	var isIpad = __webpack_require__(10);
+	var isMac = __webpack_require__(11);
+	var isWP = __webpack_require__(12);
+	var isMobile = __webpack_require__(13);
+
+	var sniffFns = {
+	    android: isAndroid,
+	    mobileAndroid: isAndroidMobile,
+	    bb10: isBB,
+	    browser: {
+	      chrome: isChrome,
+	      chromeMobile: isChromeMobile,
+	      firefox: isFF,
+	      opera: isOpera,
+	      safari: isSafari
+	    },
+	    iPhone: isIphone,
+	    iPad: isIpad,
+	    mac: isMac,
+	    windowsPhone: isWP,
+	    mobile: isMobile
+	}
+
+	if (module && module.exports) {
+	  module.exports = sniffFns;
+	} else {
+	  window.dateFns = sniffFns;
+	}
+	
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(15)(module)))
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var testString = __webpack_require__(14);
+
+	var isChrome = function(str){
+	  return testString(str, 'chrome');
+	}
+
+	module.exports = isChrome;
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var testString = __webpack_require__(14);
+	var sniffChrome = __webpack_require__(1);
+	var sniffAndroid = __webpack_require__(3);
+
+	var isChromeMobile = function(str){
+	  return (sniffChrome(str) && sniffAndroid(str)) || testString(str, 'CriOS');
+	}
+
+	module.exports = isChromeMobile;
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var testString = __webpack_require__(14)
+
+	var isAndroid = function(str){
+	  if (testString(str, 'android')) {
+	    return parseFloat(str.replace(/^.* android (\d+)\.(\d+).*$/, "$1.$2")) || true
+	  } else {
+	    return false
+	  }
+	};
+
+	module.exports = isAndroid;
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var testString = __webpack_require__(14);
+	var sniffAndroid = __webpack_require__(3);
+
+	var isMobileAndroid = function(str){
+	  return sniffAndroid(str) && testString(str, 'mobile');
+	}
+
+	module.exports = isMobileAndroid;
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var testString = __webpack_require__(14);
+
+	var isBB = function(str){
+	  return testString(str, 'bb10');
+	}
+
+	module.exports = isBB;
+
+
+/***/ },
+/* 6 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var testString = __webpack_require__(14);
+
+	var isFirefox = function(str){
+	  return testString(str, 'firefox');
+	}
+
+	module.exports = isFirefox;
+
+
+/***/ },
+/* 7 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var testString = __webpack_require__(14);
+
+	var isOpera = function(str){
+	  return testString(str, 'OPR/') || testString(str, 'opera');
+	}
+
+	module.exports = isOpera;
+
+
+
+/***/ },
+/* 8 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var testString = __webpack_require__(14);
+	var isChrome = __webpack_require__(1);
+	var isAndroid = __webpack_require__(3);
+
+	var isSafari = function(str){
+	  return testString(str, 'safari') && testString(str, 'apple') && !(isChrome(str) && isAndroid(str));
+	}
+
+	module.exports = isSafari;
+
+
+/***/ },
+/* 9 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var testString = __webpack_require__(14)
+
+	var isIphone = function(str){
+	  return testString(str, 'iphone;') || testString(str, 'ipod touch;')
+	};
+
+	module.exports = isIphone;
+
+
+
+/***/ },
+/* 10 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var testString = __webpack_require__(14)
+
+	var isIpad = function(str){
+	  return testString(str, 'ipad;')
+	};
+
+	module.exports = isIpad;
+
+
+
+/***/ },
+/* 11 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var testString = __webpack_require__(14)
+
+	var isMac = function(str){
+	  return testString(str, 'mac os')
+	};
+
+	module.exports = isMac;
+
+
+
+/***/ },
+/* 12 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var testString = __webpack_require__(14);
+
+	var isWindowsPhone = function(str) {
+	  if (testString(str, 'windows phone') || testString(str, 'iemobile')) {
+	    return parseFloat(str.replace(/^.* windows phone (os )?(\d+)\.(\d+).*$/, "$2.$3")) || true;
+	  } else {
+	    return false;
+	  }
+	};
+
+	module.exports = isWindowsPhone;
+
+
+/***/ },
+/* 13 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var testString = __webpack_require__(14);
+	var isAndroidMobile = __webpack_require__(4);
+	var isIphone = __webpack_require__(9);
+	var isIpad = __webpack_require__(10);
+	var isWP = __webpack_require__(12);
+	var isBB = __webpack_require__(5);
+
+	var isMobile = function(str){
+	  return isAndroidMobile(str) || isIphone(str) || isIpad(str) || isWP(str) || isBB(str);
+	};
+
+	module.exports = isMobile;
+
+
+/***/ },
+/* 14 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/**
+	 * Test string if it contains other string
+	 * @param  {string} str
+	 * @param  {string} test
+	 * @returns {boolean}
+	 */
+	var testString = function(str, test){
+	  return RegExp(test.toLowerCase()).test(str.toLowerCase())
+	}
+
+	module.exports = testString;
+
+
+/***/ },
+/* 15 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = function(module) {
+		if(!module.webpackPolyfill) {
+			module.deprecate = function() {};
+			module.paths = [];
+			// module.parent = undefined by default
+			module.children = [];
+			module.webpackPolyfill = 1;
+		}
+		return module;
+	}
+
+
+/***/ }
+/******/ ])
